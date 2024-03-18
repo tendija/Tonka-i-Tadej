86165e35f0844d4c2087f0b327cadd38c1fd2af3
Tadej Barović <105346448+tendija@users.noreply.github.com>
2024-03-18 23:01:31 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const form = document.forms['contact-form']
 form.addEventListener('submit', e => {
   e.preventDefault()
   fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-  .then(response => alert("Thank you! your form is submitted successfully." ))
+  .then(response => alert("Odgovorili ste na pozivnicu. Hvala!" ))
   .then(() => { window.location.reload(); })
   .catch(error => console.error('Error!', error.message))
 })
