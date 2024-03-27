1158bf4bdb7db2f162a442d36abbd2b48029d720
Tadej Barović <105346448+tendija@users.noreply.github.com>
2024-03-27 09:45:18 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const form = document.forms['contact-form']
 form.addEventListener('submit', e => {
   e.preventDefault()
   fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-  .then(response => alert("Odgovorili ste na pozivnicu. Hvala!" ))
+  .then(response => alert("Thank you! your form is submitted successfully." ))
   .then(() => { window.location.reload(); })
   .catch(error => console.error('Error!', error.message))
 })
