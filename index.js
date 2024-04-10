736d5b5d1c9080d9cb6eee6fa26ed280541a94cf
Tadej Barović <105346448+tendija@users.noreply.github.com>
2024-04-10 09:49:27 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const scriptURL = 'https://script.google.com/macros/s/AKfycbwuaEIenjQvhCdAOuIEHR_lsGsSyeb0iX3mmPv1Sfm8CT9eWbqEOXBtepJ2-9FLmS9k3g/exec'
 
-const form = document.forms['contact-form']
-
+const form = document.forms['contact-form'];
+var button = document.getElementById('submit');
 form.addEventListener('submit', e => {
   document.querySelector('#submit').value = 'Poslano, hvala!';
   e.preventDefault()
   fetch(scriptURL, { method: 'POST', body: new FormData(form)})
   .then(response => alert("Odgovorili ste na pozivnicu. Hvala!" ))
   .catch(error => console.error('Error!', error.message))
-  button.disabled = true
+  button.disabled = true;
 })
 
 $(function() {
